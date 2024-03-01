b09fa7681b6b4c18fd643f4b7948b7741345dd1d
bananaFish001 <raj123.rs308@gmail.com>
2024-03-02 00:27:33 +0530

    Learning about ternary operators

---
+++ ternaryOperators.js
@@ -0,0 +1,13 @@
+let result1 = (10>0) ? "I am right" : "you are right"
+
+//console.log(result)
+
+function gfg(){
+  let retirementAge = 65
+
+  let result = (retirementAge > 60) ? "You can retire" : "YOu better come to work tomorrow"
+
+  console.log(result)
+}
+
+gfg();
---
+++ ternaryOperators2.js
@@ -0,0 +1,8 @@
+function gfg(){
+  let marks = 90
+
+  let result = (marks > 90) ? "Nerd! detected" : (marks > 70 & marks < 90) ? "You are mid" : (marks < 70) ? "You need to study more" : "You failure!";
+  console.log(result);
+}
+
+gfg();
