ac0813a2e0645980ca64068328d08202a647bdea
bananaFish001 <raj123.rs308@gmail.com>
2024-03-02 21:28:36 +0530

    Arrow functions

---
+++ functions_2.js
@@ -0,0 +1,26 @@
+function myFunction(){
+  let a = 5, b = 7;
+  return a+b
+}
+
+console.log(myFunction())
+
+// Using arrow functions
+const newFunc = () => {
+  let a = 1, b = 2;
+  return a + b
+}
+
+console.log(newFunc())
+
+// Another arrow functions
+const func2 = (num) => num*num;
+
+console.log(func2(4));
+
+let obj = {
+  num: 10,
+  myFunc: () => {console.log(this.num)}
+}
+
+obj.myFunc(); // This is a limitation of the arrow fumction as it only refer to the objects within it's scope and it does not have num in it.
